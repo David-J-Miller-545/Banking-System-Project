8c00550e019780621040d3f18f3a0169fdc180eb
David Joseph Miller <56939567+ZombiKiller76er@users.noreply.github.com>
2023-11-02 18:33:25 -0400

    Created more tests for CreateValidator, accepts certain accounts, unique 8 digit id, + 1-10 float apr, and argument counts for types of accounts.

--- src/main/java/CreateValidator.java
+++ src/main/java/CreateValidator.java
@@ -1,7 +1,13 @@
 public class CreateValidator {
+	private Bank bank;
+
+	public CreateValidator(Bank bank) {
+		this.bank = bank;
+	}
 
 	public String[] readCommandArguments(String command) {
 		int count = 1;
+		command = command.toLowerCase();
 		for (int i = 0; i < command.length(); i++) {
 			if (command.charAt(i) == ' ') {
 				count++;
@@ -10,10 +16,63 @@ public class CreateValidator {
 		return command.split(" ", count);
 	}
 
+	public boolean validateIdAndAPR(String[] args) {
+		if (validateID(args[2])) {
+			return validateAPR(args[3]);
+		} else {
+			return false;
+		}
+	}
+
+	public boolean validateID(String arg3) {
+		try {
+			int ID = Integer.parseInt(arg3);
+			if ((ID / 10000000) < 10 && (ID / 10000000) > 0) {
+				return (bank.getAccount(ID) instanceof Account) ? false : true;
+			} else {
+				return false;
+			}
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public boolean validateAPR(String arg4) {
+		try {
+			double apr = Double.parseDouble(arg4);
+			return (0 <= apr && apr <= 10) ? true : false;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
 	public boolean validate(String command) {
 		String[] arguments = readCommandArguments(command);
 		if (arguments[0].equals("create")) {
-			return true;
+			if (arguments[1].equals("savings") || arguments[1].equals("checking")) {
+				if (!(arguments.length > 4 || arguments.length < 4)) {
+					if (validateIdAndAPR(arguments)) {
+						return true;
+					} else {
+						return false;
+					}
+				} else {
+					return false;
+				}
+
+			} else if (arguments[1].equals("cd")) {
+				if (!(arguments.length > 5 || arguments.length < 5)) {
+					if (validateIdAndAPR(arguments)) {
+						return true;
+					} else {
+						return false;
+					}
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
 		} else {
 			return false;
 		}
--- src/test/java/CreateValidatorTest.java
+++ src/test/java/CreateValidatorTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 public class CreateValidatorTest {
 
 	private CreateValidator createValidator;
+	private Bank bank;
 
 	@BeforeEach
 	public void setUp() {
-		createValidator = new CreateValidator();
+		bank = new Bank();
+		createValidator = new CreateValidator(bank);
 	}
 
 	@Test
@@ -29,17 +31,69 @@ public class CreateValidatorTest {
 
 	@Test
 	public void validator_accepts_savings_account() {
-
+		assertTrue(createValidator.validate("create savings 12345678 0.6"));
 	}
 
 	@Test
 	public void validator_accepts_checking_account() {
-
+		assertTrue(createValidator.validate("create checking 12345678 0.6"));
 	}
 
 	@Test
 	public void validator_accepts_cd_account() {
+		assertTrue(createValidator.validate("create cd 12345678 0.6 10000"));
+	}
 
+	@Test
+	public void validator_refuses_input_other_than_a_valid_account_for_the_second_argument() {
+		assertFalse(createValidator.validate("create notAnAccount 12345678 0.6"));
+	}
+
+	@Test
+	public void validator_is_case_insensitive() {
+		assertTrue(createValidator.validate("CrEaTe SAvIngS 12345678 0.6"));
+	}
+
+	@Test
+	public void validator_accepts_an_8_digit_id() {
+		assertTrue(createValidator.validate("create savings 12345678 0.6"));
+	}
+
+	@Test
+	public void validator_refuses_anything_but_an_8_digit_id() {
+		assertFalse(createValidator.validate("create savings 123456789 0.6"));
+	}
+
+	@Test
+	public void validator_refuses_an_existing_id() {
+		Account account = new Checking(12345678, 0);
+		bank.addAccount(account);
+		assertFalse(createValidator.validate("create savings 12345678 0.6"));
+	}
+
+	@Test
+	public void validator_accepts_a_positve_float_between_1_and_10_for_apr() {
+		assertTrue(createValidator.validate("create savings 12345678 0.6"));
+	}
+
+	@Test
+	public void validator_accepts_4_arguments_for_creating_a_checking_account() {
+		assertTrue(createValidator.validate("create checking 12345678 0.6"));
+	}
+
+	@Test
+	public void validator_refuses_more_or_less_than_4_arguments_for_creating_a_checking_account() {
+		assertFalse(createValidator.validate("create checking 12345678 0.6 Nope"));
+	}
+
+	@Test
+	public void validator_accepts_5_arguments_for_creating_a_cd_account() {
+		assertTrue(createValidator.validate("create cd 12345678 0.6 1000"));
+	}
+
+	@Test
+	public void validator_refuses_more_or_less_than_5_arguments_for_creating_a_cd_account() {
+		assertFalse(createValidator.validate("create savings 12345678 0.6 1000 Nope"));
 	}
 
 }
